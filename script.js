2ac1d4850550cdb59846a9c5384d034cb7e54cee
HappyBirthdayMyLittleLove <127628183+HappyBirthdayMyLittleLove@users.noreply.github.com>
2023-03-16 11:18:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const blockContainer = document.getElementById("blockContainer");
-const blockElement = document.querySelectorAll(".block");
+//FRONT-END ELEMENTS
+const container2 = document.getElementById("container2");
 const wandOfLightButton = document.querySelector(".wand-of-light");
-const wandTitle = document.querySelector(".wand-title");
+const cardContent = document.querySelector(".card-content");
 const greeting = document.querySelector(".greeting");
 const leftFireworks = document.querySelector(".container1");
 const rightFireworks = document.querySelector(".container3");
-
 const width =
   window.innerWidth ||
   document.documentElement.clientWidth ||
   document.body.clientWidth;
-
+//ALL COLORS USED FOR FONTS
 const gradientColors = [
   "linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)",
-  "linear-gradient(to right, #ffecd2 0%, #fcb69f 100%)",
+  // "linear-gradient(to right, #ffecd2 0%, #fcb69f 100%)",
   "linear-gradient(to right, #ff8177 0%, #ff867a 0%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%)",
   "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)",
   "linear-gradient(to right, #4facfe 0%, #00f2fe 100%)",
@@ -39,7 +38,7 @@ const plainColors = [
   "#DDA0DD",
   "#FF6347",
 ];
-
+//STYLES LANDING PAGE H1
 const greetingText = "Happy Birthday My Love";
 greetingText.split("").forEach((character) => {
   const characterSpan = document.createElement("span");
@@ -48,7 +47,7 @@ greetingText.split("").forEach((character) => {
 });
 const greetLetter = document.querySelectorAll(".greeting span");
 greetLetter.forEach((letter) => {
-  if (width > 780) {
+  if (width > 980) {
     letter.addEventListener("mouseenter", () => {
       const randomNumberLetter = Math.floor(Math.random() * plainColors.length);
       letter.style.color = plainColors[randomNumberLetter];
@@ -58,7 +57,7 @@ greetLetter.forEach((letter) => {
     letter.style.color = plainColors[randomNumberLetter];
   }
 });
-
+//STYLES "BIRTHDAY CARD" BUTTON
 const randomNumber = Math.floor(Math.random() * plainColors.length);
 const randomNumberPlain = Math.floor(Math.random() * plainColors.length);
 const randomNumberborder = Math.floor(Math.random() * plainColors.length);
@@ -79,19 +78,19 @@ wandOfLightButton.addEventListener("mouseleave", () => {
   wandOfLightButton.style.backgroundImage = "";
   wandOfLightButton.style.borderColor = "";
 });
-
+//BIRTHDAY CARD CONTENT AND STYLING
 const bdayCard = () => {
   const speech =
     "My love, I am so proud of you and the person you've become. You have unconditionally supported me from day one and showed me a love I didnt know existed. I'll always be there for you the same way you've always been there for me. I love you. -Trent";
   speech.split("").forEach((character) => {
     const characterSpan = document.createElement("span");
     characterSpan.innerText = character;
-    wandTitle.appendChild(characterSpan);
+    cardContent.appendChild(characterSpan);
   });
-  const characters = document.querySelectorAll(".wand-title span");
+  const characters = document.querySelectorAll(".card-content span");
 
   characters.forEach((letter) => {
-    if (width > 780) {
+    if (width > 1400) {
       letter.addEventListener("mouseenter", () => {
         const randomNumberLetter = Math.floor(
           Math.random() * plainColors.length
@@ -104,7 +103,7 @@ const bdayCard = () => {
     }
   });
 };
-
+//HANDLES ALL ANIMATION THAT HAPPENS AFTER PRESSING "BDAY CARD BUTTON"
 const makeAnimation = () => {
   const greetingKeyFrames = new KeyframeEffect(
     greeting,
@@ -123,66 +122,45 @@ const makeAnimation = () => {
   WOLanimation.play();
 };
 const mobileFireworks = new KeyframeEffect(
-  blockContainer,
+  container2,
   { opacity: ".5" },
   { duration: 300, fill: "forwards" }
 );
 const mobileFireworksAnimation = new Animation(mobileFireworks);
 
+//OPENS BDAY CARD AND DISPLAYS CONTENT
 const WOLSwitch = () => {
   makeAnimation();
-  if (width < 780) {
+  if (width < 480) {
     mobileFireworksAnimation.play();
-    blockContainer.style.display = "flex";
-    wandTitle.style.display = "block";
-    blockContainer.style.width = "100%";
-    blockContainer.style.height = "75%";
+    container2.style.display = "flex";
+    cardContent.style.display = "block";
+    container2.style.width = "100%";
+    container2.style.height = "75%";
+  }
+  if (width > 480 && width < 880) {
+    mobileFireworksAnimation.play();
+    container2.style.display = "flex";
+    cardContent.style.display = "block";
+    container2.style.width = "50%";
+    container2.style.height = "75%";
+  }
+  if (width > 880 && width < 1024) {
+    mobileFireworksAnimation.play();
+    container2.style.display = "flex";
+    cardContent.style.display = "block";
+    container2.style.width = "100%";
+    container2.style.height = "75%";
   }
   wandOfLightButton.disabled = "true";
-  wandTitle.innerHTML = "";
-  wandTitle.style.transition = "opacity 2000ms ease-in-out";
-  wandTitle.style.opacity = "1";
-  wandTitle.style.color = "transparent";
+  cardContent.innerHTML = "";
+  cardContent.style.transition = "opacity 2500ms ease-in-out";
+  cardContent.style.opacity = "1";
+  cardContent.style.color = "transparent";
 
   bdayCard();
 };
-
-const bomb = () => {
-  if (width > 780) {
-    wandOfLightButton.addEventListener("mousedown", () => {
-      blockElement.forEach((block) => {
-        block.style.transition = "opacity ease-in";
-        block.style.opacity = ".9";
-        const randomNumber = Math.floor(Math.random() * gradientColors.length);
-        block.style.backgroundImage = gradientColors[randomNumber];
-      });
-    });
-    wandOfLightButton.addEventListener("mouseup", () => {
-      blockElement.forEach((block) => {
-        block.style.transition = "opacity 2000ms ease-out";
-        block.style.opacity = "0";
-      });
-    });
-  } else {
-    wandOfLightButton.addEventListener("click", () => {
-      blockElement.forEach((block) => {
-        block.style.transition = "opacity ease-in";
-        block.style.opacity = ".8";
-        const randomNumber = Math.floor(Math.random() * gradientColors.length);
-        block.style.backgroundImage = gradientColors[randomNumber];
-      });
-    });
-    wandOfLightButton.addEventListener("click", () => {
-      blockElement.forEach((block) => {
-        block.style.transition = "opacity 1500ms ease-out";
-        block.style.opacity = "0";
-      });
-    });
-  }
-};
-
-bomb();
-
+//ADDS AUDIO OF ACTUAL FIREWORKS IF YOU CLICK ON FIREWORK IMAGES
 leftFireworks.addEventListener("click", () => {
   const audioFile = document.getElementById("audio-file");
   setTimeout(() => {
@@ -195,8 +173,8 @@ rightFireworks.addEventListener("click", () => {
     audioFile.play();
   }, 100);
 });
-if (width < 780) {
-  blockContainer.addEventListener("click", () => {
+if (width < 980) {
+  container2.addEventListener("click", () => {
     const audioFile = document.getElementById("audio-file");
     setTimeout(() => {
       audioFile.play();
